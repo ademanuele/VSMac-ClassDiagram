a10d5f8f75994097411811a4f8d9433ccffa308f
Arthur Demanuele <ademan19@gmail.com>
2018-10-28 18:13:17 +0100

    Import from other repository

---
+++ Diagram/Diagram/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "Diagram",
+    Namespace = "Diagram",
+    Version = "1.0"
+)]
+
+[assembly: AddinName("Diagram")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("Extension that gives diagram functionality.")]
+[assembly: AddinAuthor("Arthur Demanuele")]
